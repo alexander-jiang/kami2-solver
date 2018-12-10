4836dc987a61eacc94e6d629ceb2e8f356bed301
Alexander Jiang <dashuai99@gmail.com>
2018-12-09 22:35:11 -0600

    fix puzzle 18 graph

--- test_puzzle18.py
+++ test_puzzle18.py
@@ -3,59 +3,59 @@ import deterministic_search as search
 import informed_search
 
 puzzle18_graph = {
-    1: frozenset([2, 8]),
+    1: frozenset([2, 3, 5]),
     2: frozenset([1, 3]),
-    3: frozenset([2, 4, 8]),
-    4: frozenset([3, 5, 8]),
-    5: frozenset([4, 6]),
-    6: frozenset([5, 7, 9, 13, 17, 18, 19, 20, 26]),
-    7: frozenset([6, 9, 17]),
-    8: frozenset([1, 3, 4, 10, 11, 12, 13, 15]),
-    9: frozenset([6, 7]),
-    10: frozenset([8, 14]),
-    11: frozenset([8, 12, 14]),
-    12: frozenset([8, 11, 14, 15, 16, 18, 21, 23]),
-    13: frozenset([6, 8, 15]),
-    14: frozenset([10, 11, 12, 16]),
-    15: frozenset([8, 12, 13, 18]),
-    16: frozenset([12, 14, 21]),
-    17: frozenset([6, 7, 26]),
-    18: frozenset([6, 12, 15, 19, 20, 22, 24, 25]),
-    19: frozenset([6, 18]),
-    20: frozenset([6, 18]),
-    21: frozenset([12, 16]),
-    22: frozenset([18, 23]),
-    23: frozenset([12, 22]),
-    24: frozenset([18,]),
-    25: frozenset([18,]),
-    26: frozenset([6, 17])
+    3: frozenset([1, 2, 4, 5]),
+    4: frozenset([3, 5, 16, 17]),
+    5: frozenset([1, 3, 4, 6, 7, 10, 13, 14]),
+    6: frozenset([5, 7, 8]),
+    7: frozenset([5, 6, 8, 10]),
+    8: frozenset([6, 7, 9, 10]),
+    9: frozenset([8, 10, 11]),
+    10: frozenset([5, 7, 8, 9, 11, 12, 13, 23, 24]),
+    11: frozenset([9, 10]),
+    12: frozenset([10, 24]),
+    13: frozenset([5, 10, 14, 16, 21, 23]),
+    14: frozenset([5, 13, 16]),
+    15: frozenset([16, 18]),
+    16: frozenset([4, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23]),
+    17: frozenset([4, 16]),
+    18: frozenset([15, 16, 19]),
+    19: frozenset([16, 18, 20]),
+    20: frozenset([16, 19, 22]),
+    21: frozenset([13, 16, 23]),
+    22: frozenset([16, 20, 23]),
+    23: frozenset([10, 13, 16, 21, 22, 24, 25, 26]),
+    24: frozenset([10, 12, 23, 25]),
+    25: frozenset([23, 24, 26]),
+    26: frozenset([23, 25])
 }
 puzzle18_node_colors = {
     1: 'w',
     2: 'o',
     3: 'b',
     4: 'w',
-    5: 'o',
-    6: 'b',
-    7: 'w',
+    5: 'c',
+    6: 'w',
+    7: 'o',
     8: 'c',
     9: 'o',
-    10: 'w',
-    11: 'o',
-    12: 'b',
-    13: 'o',
-    14: 'c',
-    15: 'w',
-    16: 'o',
-    17: 'c',
-    18: 'o',
+    10: 'b',
+    11: 'w',
+    12: 'o',
+    13: 'w',
+    14: 'o',
+    15: 'o',
+    16: 'b',
+    17: 'o',
+    18: 'w',
     19: 'c',
-    20: 'c',
-    21: 'w',
-    22: 'w',
+    20: 'w',
+    21: 'c',
+    22: 'c',
     23: 'o',
-    24: 'b',
-    25: 'w',
+    24: 'w',
+    25: 'b',
     26: 'w'
 }
 puzzle18_moves_left = 5
@@ -63,14 +63,14 @@ puzzle18_moves_left = 5
 puzzle18_step0 = kami2.PuzzleState(puzzle18_graph, puzzle18_node_colors, puzzle18_moves_left)
 puzzle18 = kami2.Kami2Puzzle(puzzle18_step0)
 
-# print("Solving using DFS:")
-# search.DepthFirstSearch().solve(puzzle18)
+print("Solving using DFS:")
+search.DepthFirstSearch().solve(puzzle18)
 #
 # print("Solving using UCS:")
 # search.UniformCostSearch().solve(puzzle18)
 
-print("Solving using A* (# colors heuristic):")
-informed_search.AStarSearch(informed_search.num_colors_heuristic).solve(puzzle18)
+# print("Solving using A* (# colors heuristic):")
+# informed_search.AStarSearch(informed_search.num_colors_heuristic).solve(puzzle18)
 
 # color distance heuristic is too slow...
 # print("Solving using A* (color distance heuristic):")
