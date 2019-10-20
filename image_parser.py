8063f18acfad7abb6a1f1b99055e0fb19a9cae25
Alexander Jiang <dashuai99@gmail.com>
2019-10-20 00:00:14 -0700

    Use bilateral filter to take out noise
    some pixels from the triangle edges in the original image were being
    labeled incorrectly with kmeans.
    using bilateral filter to preprocess should make this less likely to
    occur, and since the image already has fairly distinct colors, the
    diameter (d) of the bilateral filter doesn't have to be large.

--- image_parser.py
+++ image_parser.py
@@ -9,11 +9,19 @@ img = cv2.imread("kami2puzzle18.png", cv2.IMREAD_COLOR)
 puzzle_height_y = 1210
 puzzle_width_x = img.shape[1]
 
-puzzle = copy.deepcopy(img)
+bilateral_d = 5
+bilateral_sigma = 100
+bilateral_filtered_img = cv2.bilateralFilter(img, bilateral_d, bilateral_sigma, bilateral_sigma)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+puzzle = copy.deepcopy(bilateral_filtered_img)
 puzzle = puzzle[0:puzzle_height_y, 0:puzzle_width_x]
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title("original image")
-plt.show()
+ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+ax1.set_title("original image")
+
+ax2.imshow(cv2.cvtColor(bilateral_filtered_img, cv2.COLOR_BGR2RGB))
+ax2.set_title(f"bilateral filter (d={bilateral_d}, sigma={bilateral_sigma})")
+# plt.show()
 
 print(puzzle.shape) # y by x by color
 print(puzzle[0,0:5,:])
@@ -60,7 +68,7 @@ ax1.set_title(f"k means colors (k = {K})")
 ax2.imshow(cv2.cvtColor(palette, cv2.COLOR_BGR2RGB))
 ax2.axis('off')
 ax2.set_title("color palette from original image")
-plt.show()
+# plt.show()
 
 flatten_labels = np.ravel(labels)
 print("flattened labels:")
@@ -81,4 +89,53 @@ ax1.axis('off')
 ax2.imshow(cv2.cvtColor(converted_puzzle, cv2.COLOR_BGR2RGB))
 ax2.set_title(f"converted image ({K} colors)")
 ax2.axis('off')
+# plt.show()
+
+# gets a list of neighboring pixel coordinates (y, x)
+def get_neighbor_coords(pixel_y, pixel_x):
+    neighbor_coords = []
+    if pixel_y > 0:
+        neighbor_coords.append((pixel_y - 1, pixel_x))
+    if pixel_y < puzzle_height_y - 1:
+        neighbor_coords.append((pixel_y + 1, pixel_x))
+    if pixel_x > 0:
+        neighbor_coords.append((pixel_y, pixel_x - 1))
+    if pixel_x < puzzle_width_x - 1:
+        neighbor_coords.append((pixel_y, pixel_x + 1))
+    return neighbor_coords
+
+# parse contiguous regions (i.e. nodes) from converted colors
+pixel_labels = np.reshape(flatten_labels, (puzzle_height_y, puzzle_width_x))
+pixel_nodes = np.zeros((puzzle_height_y, puzzle_width_x))
+next_node_number = 1
+for pixel_y in range(puzzle_height_y):
+    for pixel_x in range(puzzle_width_x):
+        # if pixel was already marked with a node number, then don't change it
+        if pixel_nodes[pixel_y, pixel_x] > 0:
+            continue
+
+        print(f"new node {next_node_number}: starting with pixel (y={pixel_y}, x={pixel_x})")
+        pixel_nodes[pixel_y, pixel_x] = next_node_number
+        next_node_number += 1
+
+        neighbor_coords = get_neighbor_coords(pixel_y, pixel_x)
+
+        # recursively assign the same node number to all contiguous pixels that are the same color
+        while len(neighbor_coords) > 0:
+            nbr_y, nbr_x = neighbor_coords.pop(0)
+            if pixel_labels[nbr_y, nbr_x] == pixel_labels[pixel_y, pixel_x] and pixel_nodes[nbr_y, nbr_x] == 0:
+                pixel_nodes[nbr_y, nbr_x] = pixel_nodes[pixel_y, pixel_x]
+                neighbor_coords.extend(get_neighbor_coords(nbr_y, nbr_x))
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+c = ax1.pcolor(pixel_nodes)
+ax1.invert_yaxis()
+ax1.set_title("node groupings")
+fig.colorbar(c, ax=ax1)
+
+ax2.imshow(cv2.cvtColor(puzzle, cv2.COLOR_BGR2RGB))
+ax2.set_title("original image")
 plt.show()
+
+# there are like 2 pixels labeled incorrectly: (y=954, x=371)
