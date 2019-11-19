ad441f80d5403d2f0a22fc859f471bcd963f17e3
Alexander Jiang <dashuai99@gmail.com>
2019-11-19 01:34:26 -0800

    image parser test working

--- image_parser.py
+++ image_parser.py
@@ -1,5 +1,7 @@
 import argparse
+import collections
 import copy
+import json
 import numpy as np
 import cv2
 import matplotlib
@@ -11,6 +13,20 @@ puzzle_width_x = 750
 
 def get_original_image(filename):
     return cv2.imread(filename, cv2.IMREAD_COLOR)
+
+def image_preprocessing(img):
+    bilateral_d = 5
+    bilateral_sigma = 100
+    bilateral_filtered_img = cv2.bilateralFilter(img, bilateral_d, bilateral_sigma, bilateral_sigma)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax1.set_title("original image")
+
+    ax2.imshow(cv2.cvtColor(bilateral_filtered_img, cv2.COLOR_BGR2RGB))
+    ax2.set_title(f"bilateral filter (d={bilateral_d}, sigma={bilateral_sigma})")
+    # plt.show()
+    return bilateral_filtered_img
 
 def crop_to_puzzle(full_screenshot_img):
     puzzle = copy.deepcopy(full_screenshot_img)
@@ -63,42 +79,8 @@ def assign_pixels_to_nodes(labels):
                     neighbor_coords.extend(get_neighbor_coords(nbr_y, nbr_x))
     return (pixel_nodes, next_node_number - 1)
 
-# gets a list of neighboring pixel coordinates (y, x)
-def get_neighbor_coords(pixel_y, pixel_x):
-    neighbor_coords = []
-    if pixel_y > 0:
-        neighbor_coords.append((pixel_y - 1, pixel_x))
-    if pixel_y < puzzle_height_y - 1:
-        neighbor_coords.append((pixel_y + 1, pixel_x))
-    if pixel_x > 0:
-        neighbor_coords.append((pixel_y, pixel_x - 1))
-    if pixel_x < puzzle_width_x - 1:
-        neighbor_coords.append((pixel_y, pixel_x + 1))
-    return neighbor_coords
-
-def main():
-    parser = argparse.ArgumentParser(description='Given a screenshot of a Kami 2 puzzle, construct the graph representation of the puzzle state.')
-    parser.add_argument('img_filename', type=str,
-                        help='path to the screenshot')
-
-    args = parser.parse_args()
-    print(args.img_filename)
-
-    img = get_original_image(args.img_filename)
-
-    bilateral_d = 5
-    bilateral_sigma = 100
-    bilateral_filtered_img = cv2.bilateralFilter(img, bilateral_d, bilateral_sigma, bilateral_sigma)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    ax1.set_title("original image")
-
-    ax2.imshow(cv2.cvtColor(bilateral_filtered_img, cv2.COLOR_BGR2RGB))
-    ax2.set_title(f"bilateral filter (d={bilateral_d}, sigma={bilateral_sigma})")
-    # plt.show()
-
-    puzzle = crop_to_puzzle(bilateral_filtered_img)
+def label_pixels_by_node(preprocessed_img, debug_print=False):
+    puzzle = crop_to_puzzle(preprocessed_img)
     # print(puzzle.shape) # y by x by color
     # print(puzzle[0,0:5,:])
 
@@ -117,14 +99,16 @@ def main():
     flags = cv2.KMEANS_RANDOM_CENTERS
     random_restarts = 10
 
-    print(f"starting K means: max {max_iters} iterations, {random_restarts} random restarts")
+    if debug_print:
+        print(f"starting K means: max {max_iters} iterations, {random_restarts} random restarts")
     compactness, labels, centers = cv2.kmeans(pixel_colors, K, None, termination_criteria, random_restarts, flags)
-    print("K means complete!")
-    # print("compactness =", compactness)
-    print("centers =", centers)
+    if debug_print:
+        print("K means complete!")
+        # print("compactness =", compactness)
+        print("centers =", centers)
 
     # extract the color palette
-    palette = crop_to_color_palette(bilateral_filtered_img)
+    palette = crop_to_color_palette(preprocessed_img)
 
     # compare the k-means colors to the color palette
     fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -157,20 +141,96 @@ def main():
 
     # parse contiguous regions (i.e. nodes) from converted colors
     pixel_nodes, num_nodes = assign_pixels_to_nodes(labels)
-    print(f"assigned pixels to contiguous nodes! there are {num_nodes} nodes")
+    return (pixel_nodes, num_nodes)
+
+# gets a list of neighboring pixel coordinates (y, x)
+def get_neighbor_coords(pixel_y, pixel_x):
+    neighbor_coords = []
+    if pixel_y > 0:
+        neighbor_coords.append((pixel_y - 1, pixel_x))
+    if pixel_y < puzzle_height_y - 1:
+        neighbor_coords.append((pixel_y + 1, pixel_x))
+    if pixel_x > 0:
+        neighbor_coords.append((pixel_y, pixel_x - 1))
+    if pixel_x < puzzle_width_x - 1:
+        neighbor_coords.append((pixel_y, pixel_x + 1))
+    return neighbor_coords
+
+def identify_adjacent_nodes(pixel_nodes, num_nodes, debug_print=False):
+    puzzle_graph = {}
+    counts = {}
+    for i in range(1, num_nodes + 1):
+        puzzle_graph[i] = set([])
+        counts[i] = collections.Counter()
+
+    for i in range(pixel_nodes.shape[0]):
+        for j in range(pixel_nodes.shape[1]):
+            this_node = pixel_nodes[i, j]
+
+            if i > 0 and pixel_nodes[i-1, j] != this_node:
+                counts[this_node][pixel_nodes[i-1, j]] += 1
+            if i < pixel_nodes.shape[0] - 1 and pixel_nodes[i+1, j] != this_node:
+                counts[this_node][pixel_nodes[i+1, j]] += 1
+            if j > 0 and pixel_nodes[i, j-1] != this_node:
+                counts[this_node][pixel_nodes[i, j-1]] += 1
+            if j < pixel_nodes.shape[1] - 1 and pixel_nodes[i, j+1] != this_node:
+                counts[this_node][pixel_nodes[i, j+1]] += 1
+
+
+    for this_node in range(1, num_nodes + 1):
+        for potential_nbr in counts[this_node]:
+            # set a reasonable threshold to avoid two opposit "corners" being marked as adjacent (corner cases, literally!)
+            if counts[this_node][potential_nbr] > 5:
+                puzzle_graph[this_node].add(potential_nbr)
+                puzzle_graph[potential_nbr].add(this_node)
+            if debug_print:
+                print(f"neighbors of node {this_node} = {puzzle_graph[this_node]}")
+
+    for this_node in range(1, num_nodes + 1):
+        puzzle_graph[this_node] = frozenset(puzzle_graph[this_node])
+    return puzzle_graph
+
+def parse_image_graph(img_filename, debug_print=False, debug_plots=False):
+    img = get_original_image(img_filename)
+
+    preprocessed_img = image_preprocessing(img)
+
+    pixel_nodes, num_nodes = label_pixels_by_node(preprocessed_img, debug_print=debug_print)
+    if debug_print:
+        print(f"assigned pixels to contiguous nodes! there are {num_nodes} nodes")
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    c = ax1.pcolor(pixel_nodes)
+    c = ax1.pcolormesh(pixel_nodes, cmap='jet')
     ax1.invert_yaxis()
     ax1.set_title("node groupings")
     fig.colorbar(c, ax=ax1)
 
+    puzzle = crop_to_puzzle(preprocessed_img)
     ax2.imshow(cv2.cvtColor(puzzle, cv2.COLOR_BGR2RGB))
     ax2.set_title("original image")
-    plt.show()
+    if debug_plots:
+        plt.show()
 
-    # there are like 2 pixels labeled incorrectly: (y=954, x=371)
-    # the bilateral filter should take care of it... now we need to detect
-    # which nodes are adjacent to build the graph
+    # detect which nodes are adjacent to build the graph
+    puzzle_graph = identify_adjacent_nodes(pixel_nodes, num_nodes, debug_print=debug_print)
+
+    # dump to json for debugging
+    # puzzle_graph_copy = {}
+    # for i in range(1, num_nodes + 1):
+    #     puzzle_graph_copy[i] = list(puzzle_graph[i])
+    # with open("output_graph.json", 'w') as f:
+    #     json.dump(puzzle_graph_copy, f)
+
+    return puzzle_graph
+
+def main():
+    parser = argparse.ArgumentParser(description='Given a screenshot of a Kami 2 puzzle, construct the graph representation of the puzzle state.')
+    parser.add_argument('img_filename', type=str,
+                        help='path to the screenshot')
+
+    args = parser.parse_args()
+    print(args.img_filename)
+
+    parse_image_graph(args.img_filename, debug_print=True, debug_plots=True)
 
 if __name__ == "__main__": main()
--- test_image_parser.py
+++ test_image_parser.py
@@ -1,10 +1,29 @@
 import unittest
+from test_puzzle18 import puzzle18_graph, puzzle18_node_colors
+from image_parser import parse_image_graph
+import networkx as nx
 
 class TestImageParser(unittest.TestCase):
     def test_parse_puzzle18(self):
-        # TODO check that the number of nodes is correct
-        # TODO check that the edges in the graph are correct (isomorphism?)
-        pass
+        # call the parser
+        puzzle_img = "kami2puzzle18.png"
+        print(f"parsing puzzle image: {puzzle_img}")
+        parsed_graph = parse_image_graph(puzzle_img)
+
+        # convert to dict of lists for isomorphism check
+        for node in puzzle18_graph:
+            puzzle18_graph[node] = list(puzzle18_graph[node])
+        for node in parsed_graph:
+            parsed_graph[node] = list(parsed_graph[node])
+
+        # check that the number of nodes is correct
+        self.assertEqual(len(puzzle18_graph), len(parsed_graph))
+
+        # check that the edges in the graph are correct (i.e. graphs are isomorphic)
+        puzzle18_nxgraph = nx.from_dict_of_lists(puzzle18_graph)
+        parsed_nxgraph = nx.from_dict_of_lists(parsed_graph)
+
+        self.assertTrue(nx.is_isomorphic(puzzle18_nxgraph, parsed_nxgraph))
 
     # TODO check that the image parser can handle a puzzle screenshot with blank space
 
